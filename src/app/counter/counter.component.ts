7de4d7c5b4f6ef67ab4f4b5faa4a94cf8ba53222
AdrianS2002 <aschiop2002@yahoo.com>
2025-01-20 14:29:49 +0200

    added course

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject, Inject, NgZone, OnInit, signal } from '@angular/core';
+
+import { InfoMessageComponent } from '../info-message/info-message.component';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+  imports: [InfoMessageComponent],
+})
+export class CounterComponent implements OnInit {
+  count = signal(0);
+  private zone = inject(NgZone);
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setTimeout(() => {this.count.set(0);}, 4000);
+    setTimeout(() => {console.log('time expired');}, 5000);
+  }
+
+  onDecrement() {
+    this.count.update((prevCount) => prevCount - 1);
+  }
+
+  onIncrement() {
+    this.count.update((prevCount) => prevCount + 1);
+  }
+}
